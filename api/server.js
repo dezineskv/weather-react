504324ae17ff98e4a5419c4aeb639824cb93caac
KimV <71965590+dezineskv@users.noreply.github.com>
2025-09-24 17:59:10 -0700

    vercel update

--- api/server.js
+++ api/server.js
@@ -28,14 +28,14 @@ app.use(express.json());
 // Proxy route to external API
 app.get("/weather", async (req, res) => {
   const city = req.query.q || "";
-  const apiKey = process.env.VITE_API_KEY;
+  const apiKey = process.env.API_KEY;
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
   try {
     const response = await fetch(url);
     const data = await response.json();
-    res.json(data);
-  } catch (error) {
+    res.status(200).json(data);
+  } catch (err) {
     res.status(500).json({ error: "Failed to fetch weather data" });
   }
 });
